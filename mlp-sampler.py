cc093c232ef9728a1d6632b0f85a92078076861f
Andrew Pozzuoli <apozzuoli98@gmail.com>
2023-05-11 16:01:59 -0400

    Add files via upload

--- mlp-sampler.py
+++ mlp-sampler.py
@@ -5,15 +5,145 @@ import pickle
 import streamlit as st
 
 
+# ----------------------------------------------------------------
+class Linear:
+    """
+    Linear layer building block
+    """
+    def __init__(self, fan_in, fan_out, bias=True):
+        self.weight = torch.randn((fan_in, fan_out)) / fan_in**0.5 # note: kaiming init
+        self.bias = torch.zeros(fan_out) if bias else None
+
+    def __call__(self, x):
+        self.out = x @ self.weight
+        if self.bias is not None:
+            self.out += self.bias
+        return self.out
+    
+    def parameters(self):
+        return [self.weight] + ([] if self.bias is None else [self.bias])
+
+# ----------------------------------------------------------------------
+class BatchNorm1d:
+    """
+    Batch normalization layer building block
+    """
+    def __init__(self, dim, eps=1e-5, momentum=0.1):
+        self.eps = eps
+        self.momentum = momentum
+        self.training = True
+        # parameters (trained with backprop)
+        self.gamma = torch.ones(dim)
+        self.beta = torch.zeros(dim)
+        # buffers (trained with a running 'momentum update')
+        self.running_mean = torch.zeros(dim)
+        self.running_var = torch.ones(dim)
+
+    def __call__(self, x):
+        # calculate the forward pass
+        if self.training:
+            if x.ndim == 2:
+                dim = 0
+            elif x.ndim == 3:
+                dim = (0, 1)
+            xmean = x.mean(dim, keepdim=True) # batch mean
+            xvar = x.var(dim, keepdim=True) # batch variance
+        else:
+            xmean = self.running_mean
+            xvar = self.running_var
+        xhat = (x - xmean) / torch.sqrt(xvar + self.eps) # normalize to unit variance
+        self.out = self.gamma * xhat + self.beta
+        # update the buffers
+        if self.training:
+            with torch.no_grad():
+                self.running_mean = (1 - self.momentum) * self.running_mean + self.momentum * xmean
+                self.running_var = (1 - self.momentum) * self.running_var + self.momentum * xvar
+        return self.out
+    
+    def parameters(self):
+        return [self.gamma, self.beta]
+    
+# -------------------------------------------------------------------------
+class Tanh:
+    """
+    Tanh activation layer
+    """
+    def __call__(self, x):
+        self.out = torch.tanh(x)
+        return self.out
+    
+    def parameters(self):
+        return []
+
+# -----------------------------------------------------------------------
+class Embedding:
+    """
+    Embedding layer
+    """
+    def __init__(self, num_embeddings, embedding_dim):
+        self.weight = torch.randn((num_embeddings, embedding_dim))
+
+    def __call__(self, IX):
+        self.out = self.weight[IX]
+        return self.out
+    
+    def parameters(self):
+        return [self.weight]
+    
+# ----------------------------------------------------------------------
+class FlattenConsecutive:
+    """
+    Flatten layer
+    """
+    def __init__(self, n):
+        self.n = n
+
+    def __call__(self, x):
+        B, T, C = x.shape
+        x = x.view (B, T//self.n, C*self.n)
+        if x.shape[1] == 1:
+            x = x.squeeze(1)
+
+        self.out = x
+        return self.out
+    
+    def parameters(self):
+        return []
+    
+# ------------------------------------------------------------
+class Sequential:
+    """
+    Call all layers in sequence
+    """
+    def __init__(self, layers):
+        self.layers = layers
+
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        self.out = x
+        return self.out
+
+    def parameters(self):
+        # get parameters of all layers and stretch them out into one list
+        return [p for layer in self.layers for p in layer.parameters()]
+    
+
+# ---------------------------------------------------
+
 class MLPSampler:
     def __init__(self):
+        """Samples from a trained MLP and displays the results"""
+
+        # load the mlp
         with open('mlp.pkl', 'rb') as f:
             data = pickle.load(f)
 
-        self.parameters = data['parameters']
+        self.model = data['model']
         self.block_size = data['block_size']
         self.itos = data['itos']
 
+        # display form in streamlit
         st.title("Fantasy/Sci-Fi Name Generator")
         form = st.form(key="user_settings")
         with form:
@@ -21,48 +151,38 @@ class MLPSampler:
             generate_button = form.form_submit_button("Generate Names")
             if generate_button:
                 st.markdown("---")
-                names = self.sample(num_input, self.parameters, self.block_size)
+                # sample names
+                names = self.sample(num_input, self.model, self.block_size)
                 for name in names:
                     st.write(name)
-    
-    def sample(self, num_samples, parameters, block_size):
+
+
+    def sample(self, num_samples, model, block_size):
+        """
+        Sample from the model
+        """
         seed = random.randint(1, 210000000)
-        # seed = 42
-
-        # Interesting seeds
-        # SEED:102520266
-        g = torch.Generator().manual_seed(seed)
-
+        torch.manual_seed(seed)
         names = []
-
-        C = parameters[0]
-        W1 = parameters[1]
-        b1 = parameters[2]
-        W2 = parameters[3]
-        b2 = parameters[4]
 
         for _ in range(num_samples):
             out = []
-            context = [0] * block_size
+            context = [0] * block_size # initialize with all ...
             while True:
-                emb = C[torch.tensor([context])] # (1, block_size, d)
-                h = torch.tanh(emb.view(1, -1) @ W1 + b1)
-                logits = h @ W2 + b2
+                # forward pass the neural net
+                logits = model(torch.tensor([context])) # embed the characters
                 probs = F.softmax(logits, dim=1)
-                ix = torch.multinomial(probs, num_samples=1, generator=g).item()
+                # sample from the distribution
+                ix = torch.multinomial(probs, num_samples=1).item()
+                # shift the context window and track the samples
                 context = context[1:] + [ix]
                 out.append(ix)
+                # if we sample the special '.' token, break
                 if ix == 0:
                     break
             names.append(''.join(self.itos[i] for i in out[:-1]))
             names[-1] = names[-1].capitalize()
-        #     print(''.join(self.itos[i] for i in out))
-        # print(f'\nSEED:{seed}')
-
         return names
-
-    
-
 
 
 if __name__ == '__main__':
